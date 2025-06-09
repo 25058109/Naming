c1bd553c03e354cb9af4f3cc1903b8f009390c6d
25058109 <25058109@mvula.univen.ac.za>
2025-06-09 16:08:07 +0200

    Third code

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,18 @@
+public class PrimeNumbers {
+
+     
+     
+    public static void main(String[] args) {
+         
+         for(int i=0; i <=n; i++){
+             if( n<= 1) returns fa
+                 
+                 
+             System.out.println(i);
+             {
+    
+       
+}
+}
+    }
+}
